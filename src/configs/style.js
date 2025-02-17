d50355b6b2fdc288a98e8c52809f3b2f5a8ca70c
Mikel <11315147+miorso@users.noreply.github.com>
2025-02-17 19:41:47 +0100

    Create the Styling ESLint configuration

---
+++ src/configs/style.js
@@ -0,0 +1,44 @@
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import stylistic from '@stylistic/eslint-plugin';
+
+/**
+ * @typedef {Object} ESLintConfigStyleOptions
+ * @property {boolean} [react=false] - Include JSX config for @stylistic.
+ */
+
+/** @type {ESLintConfigStyleOptions} */
+const DEFAULT_OPTIONS = {
+	react: false,
+};
+
+/**
+ * Generates an ESLint config based on style rules.
+ * @param {ESLintConfigStyleOptions} options - An object with booleans indicating which configs to include.
+ * @returns {import('eslint').Linter.Config[]} The combined ESLint configuration.
+ */
+export function getConfig(options = {}) {
+	const mergedOptions = {
+		react: options.react ?? DEFAULT_OPTIONS.react,
+	};
+
+	return [
+		eslintPluginPrettierRecommended,
+		{
+			name: '@stylistic/recommended',
+			...stylistic.configs.customize({
+				flat: true,
+				indent: 'tab',
+				quotes: 'single',
+				semi: true,
+				jsx: mergedOptions.react,
+			}),
+		},
+		{
+			name: '@stylistic/custom-rules',
+			rules: {
+				// Disallow mixed spaces and tabs for indentation
+				'@stylistic/no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+			},
+		},
+	];
+}
