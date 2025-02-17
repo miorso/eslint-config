650973e443190cb7c5a651a5ca0b26094ed6539c
Mikel <11315147+miorso@users.noreply.github.com>
2025-02-17 19:42:02 +0100

    Create the React ESLint configuration

---
+++ src/configs/react.js
@@ -0,0 +1,47 @@
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{
+		name: 'eslint-plugin-react/recommended',
+		...pluginReact.configs.flat.recommended,
+		languageOptions: {
+			...pluginReact.configs.flat.recommended.languageOptions,
+			globals: {
+				...globals.serviceworker,
+			},
+		},
+	},
+	{
+		name: 'eslint-plugin-react/jsx-runtime',
+		...pluginReact.configs.flat['jsx-runtime'],
+		languageOptions: {
+			...pluginReact.configs.flat['jsx-runtime'].languageOptions,
+			globals: {
+				...globals.serviceworker,
+			},
+		},
+	},
+	{
+		...jsxA11yPlugin.flatConfigs.recommended,
+		languageOptions: {
+			...jsxA11yPlugin.flatConfigs.recommended.languageOptions,
+			globals: {
+				...globals.serviceworker,
+				...globals.browser,
+			},
+		},
+	},
+	{
+		name: 'eslint-plugin-react-hooks/recommended',
+		plugins: {
+			'react-hooks': pluginReactHooks,
+		},
+		rules: { ...pluginReactHooks.configs.recommended.rules },
+	},
+	...pluginQuery.configs['flat/recommended'],
+];
